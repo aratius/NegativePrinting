ff6501045259e7a083962dc0937deabf430d3609
aratius <arata1129matsumotoa@gmail.com>
2023-11-20 05:07:31 +0900

    update youtube

--- src/app/page.js
+++ src/app/page.js
@@ -2,139 +2,138 @@
 'use client';
 
 import { useEffect, useRef, useState } from 'react';
-import styles from './page.module.css';
 import Youtube from 'react-youtube';
 import DIARIES from '@/const/diary';
 
 const playlists = [
   {
     date: '0902',
-    videoId: 'ZDHbZB0q_z4',
+    videoId: 'zSN_3WRS0r0',
     note: 'https://note.com/aualrxse/n/n49ae21719f46?sub_rt=share_crp'
   },
   {
     date: '0903',
-    videoId: 'LQj_CKQT_W0',
+    videoId: 'ewt0J_BSR_E',
     note: 'https://note.com/aualrxse/n/n3d02efcfd643?sub_rt=share_crp'
   },
   {
     date: '0904',
-    videoId: 'sbG0VhJ5618',
+    videoId: 'cikPU_fCAH8',
     note: 'https://note.com/aualrxse/n/nd05f9a02fe2c?sub_rt=share_crp'
   },
   {
     date: '0905',
-    videoId: 'MtOLx-IBPaE',
+    videoId: 'guDzeER7RjI',
     note: 'https://note.com/aualrxse/n/ncee05d3add9e?sub_rt=share_crp'
   },
   {
     date: '0906-0910',
-    videoId: 'ni4TJ6xcFMQ',
+    videoId: '_XosdLBt9vc',
     note: 'https://note.com/aualrxse/n/nb3921dc7c13e?sub_rt=share_crp'
   },
   {
     date: '0911',
-    videoId: 'W8VAPi0xahs',
+    videoId: 'sLQVkIbVun4',
     note: 'https://note.com/aualrxse/n/n44485284207e?sub_rt=share_crp'
   },
   {
     date: '0912',
-    videoId: 'F-PDSTaEoDk',
+    videoId: 'AkaJMVZld04',
     note: 'https://note.com/aualrxse/n/nceb7d0281c3f?sub_rt=share_crp'
   },
   {
     date: '0913',
-    videoId: 'nc3fzA6aqcE',
+    videoId: 'qG38FfwB1X4',
     note: 'https://note.com/aualrxse/n/nd5ae1b2c4d62?sub_rt=share_crp'
   },
   {
     date: '0914',
     videoId: '',
-    note: 'https://note.com/aualrxse/n/na83548e5eb09?sub_rt=share_crp'
+    node: 'https://note.com/aualrxse/n/na83548e5eb09?sub_rt=share_crp'
   },
   {
     date: '0915',
-    videoId: 'lQiUNV7-WBg',
+    videoId: 'kw5D-B49LOs',
     note: 'https://note.com/aualrxse/n/nde3854c42a4d?sub_rt=share_crp'
   },
   {
     date: '0916',
-    videoId: 'zSlOecHpM2A',
+    videoId: 'd4_FkObm6Vs',
     note: 'https://note.com/aualrxse/n/nc98a58a37439?sub_rt=share_crp'
   },
   {
     date: '0917',
-    videoId: 'GHJTm48p2o8',
+    videoId: 'vDswHcTBzYM',
     note: 'https://note.com/aualrxse/n/n6d16c0fc22b0?sub_rt=share_crp'
   },
   {
     date: '0918',
-    videoId: 'M_vLyGfmKHE',
+    videoId: 'lZrYVpWBV3g',
     note: 'https://note.com/aualrxse/n/n8cfa53442e0e?sub_rt=share_crp'
   },
   {
     date: '0919',
-    videoId: 'jOz4RA8gSQQ',
+    videoId: '-xwHQeEP_UM',
     note: 'https://note.com/aualrxse/n/n72689ee7fbbd?sub_rt=share_crp'
   },
   {
     date: '0920',
-    videoId: 'i2WVrdLaX1Y',
+    videoId: '59yFXu5beAI',
     note: 'https://note.com/aualrxse/n/ne5517d80ecce?sub_rt=share_crp'
   },
   {
     date: '0921',
     videoId: '',
-    note: 'https://note.com/aualrxse/n/nf48818b17450?sub_rt=share_crp'
+    node: 'https://note.com/aualrxse/n/nf48818b17450?sub_rt=share_crp'
   },
   {
     date: '0922',
-    videoId: 'Es0l5F2Gd78',
+    videoId: 'bq3vwCjMf84',
     note: 'https://note.com/aualrxse/n/n03955147dc23?sub_rt=share_crp'
   },
   {
     date: '0923',
-    videoId: 'T13_JfwoUbY',
+    videoId: 'YbTTlkOiZbI',
     note: 'https://note.com/aualrxse/n/n5e7fbb6a31a8?sub_rt=share_crp'
   },
   {
     date: '0924',
-    videoId: 'oJq-k6VHbak',
+    videoId: 'jdYluBNZZ8s',
     note: 'https://note.com/aualrxse/n/nd75b1fc9be1b?sub_rt=share_crp'
   },
   {
     date: '0925',
-    videoId: 'JrxvikAFZ5o',
+    videoId: 'dxhdxshLLPM',
     note: 'https://note.com/aualrxse/n/n9185fb13405b?sub_rt=share_crp'
   },
   {
     date: '0926',
-    videoId: 'X4mJQhLJ4zE',
+    videoId: 'qs2ak4K9wms',
     note: 'https://note.com/aualrxse/n/n49878778d5b4?sub_rt=share_crp'
   },
   {
     date: '0927',
-    videoId: 'DmV3-xrzaBU',
+    videoId: '5HBEaO3wG9U',
     note: 'https://note.com/aualrxse/n/nddc7b2250af7?sub_rt=share_crp'
   },
   {
     date: '0928',
-    videoId: 'pivRj1RpVLs',
+    videoId: '6E9TKc5SRXw',
     note: 'https://note.com/aualrxse/n/ndfdee850149f?sub_rt=share_crp'
   },
   {
     date: '0929',
-    videoId: 'orzde6_2K9A',
+    videoId: 'o8BjDJL_OLE',
     note: 'https://note.com/aualrxse/n/n8081056eca23?sub_rt=share_crp'
   },
   {
     date: '0930',
-    videoId: 'eqzQZod0WLU',
+    videoId: 'Vr1pIbPVL5I',
     note: 'https://note.com/aualrxse/n/n4f186c3f2bca?sub_rt=share_crp'
   },
   {
     date: '1001-1002',
-    videoId: 'gYW9NzdrLgI',
+    videoId: '7rDiESnhMKA',
     note: 'https://note.com/aualrxse/n/n9845b98d615d?sub_rt=share_crp'
   }
 ];
