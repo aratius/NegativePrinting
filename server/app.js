01b548dfd665a8a269da656045fff0959e7f786e
aratius <arata1129matsu@icloud.com>
2023-11-21 15:20:26 +0900

    wip

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const Ws = require("ws");
 const print = require('./print');
 const config = require('./config').printer1;
+const IMAGES = require('./images/js/images');
+const { getHeader, getFooter } = require("./receipt");
 
 const queue = [];
 
@@ -19,17 +21,17 @@ const onReceive = (e) => {
   console.log(value);
   if (type == 'start') {
     const { places, dates } = value;
-    queue.push({ time: 5000, text: "TODO:印刷内容" });
+    queue.push({ time: 3000, text: getHeader(places, dates) });
   } else if (type == 'end') {
     const url = value;
-    queue.push({ time: 3000, text: "TODO:印刷内容" });
+    queue.push({ time: 3000, text: getFooter(url) });
   } else if (type == 'diary') {
     const text = value;
     queue.push({ time: 1500, text: `|${text}` });
   } else if (type == 'image') {
     const date = value;
-    // const base64Str = IMAGES[date]
-    queue.push({ time: 3000, text: "TODO:印刷内容" });
+    const base64StrList = IMAGES[date];
+    queue.push({ time: 3000, text: `{image:${base64StrList[Math.floor(Math.random() * base64StrList.length)]}}` });
   }
 
   if (queue.length == 1) doQueue();
---
+++ server/receipt.js
@@ -0,0 +1,42 @@
+const getHeader = (places, dates) => {
+  const getDateStr = (date) => {
+    return `${Number(date.slice(0, 2))}/${Number(date.slice(2, 4))}/'23`;
+  };
+  let dateStr = "";
+  dateStr += getDateStr(dates[0]);
+  if (dates.length > 1) dateStr += ` - ${getDateStr(dates[dates.length - 1])}`;
+
+  return `{w:35}
+
+ ^^^\`${dateStr}\`
+
+ Negative Printing / 2023
+ arata matsumoto
+
+
+ {w:35}
+ ${places.join(" - ")}
+
+ `;
+};
+
+const getFooter = (note) => {
+  return `
+
+  {w:35}
+\===================================
+
+  {w:25,10}
+  |Subtotal | 123EUR|
+  |Tax(10%) | 456EUR|
+  |^^Total | ^^789EUR|
+
+
+  {code:${note}; option:qrcode,5,L}
+
+
+  {code:202309021002; option:code128,4,72}
+  `;
+};
+
+module.exports = { getHeader, getFooter };
